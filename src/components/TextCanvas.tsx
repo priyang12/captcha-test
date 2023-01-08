0cd7a5327a5cd7deee25312a70f813d85e035876
Priyang <patelpriyang95@gmail.com>
2023-01-08 09:36:57 +0530

    line between text

--- src/components/TextCanvas.tsx
+++ src/components/TextCanvas.tsx
@@ -8,11 +8,15 @@ function TextCanvas({
   }, [CanvasText]);
 
   const drawCaptcha = (Text: string) => {
-    const canvas = document.getElementById("captcha") as any;
-    const context = canvas?.getContext("2d");
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    const canvas = document.getElementById("captcha") as HTMLCanvasElement;
+    const context = canvas?.getContext("2d") as CanvasRenderingContext2D;
+    context?.clearRect(0, 0, canvas.width, canvas.height);
     context.font = "20px Arial";
+    context.lineWidth = 2;
     context.fillText(Text, 10, 25);
+    context.moveTo(10, 25);
+    context.lineTo(70, 10);
+    context.stroke();
   };
 
   return <canvas id="captcha" width="100" height="30"></canvas>;
